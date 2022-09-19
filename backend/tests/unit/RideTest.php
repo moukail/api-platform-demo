266f7c62efba988e839f932940d0aace35dccc56
imoukafih <i.moukafih@gmail.com>
2022-09-19 10:56:40 +0200

    * Test cant make ride with low budget * Test cant make ride with different parcel

--- backend/tests/unit/RideTest.php
+++ backend/tests/unit/RideTest.php
@@ -4,7 +4,9 @@
 namespace App\Tests\Unit;
 
 use App\Entity\Address;
+use App\Entity\Allowance;
 use App\Entity\Decision;
+use App\Entity\Parcel;
 use App\Entity\Resident;
 use App\Entity\Ride;
 use App\Entity\Taxi;
@@ -44,11 +46,21 @@ class RideTest extends \Codeception\Test\Unit
         /** @var Address $destination */
         $destination = $this->make(Address::class);
 
+        /** @var Parcel $parcel */
+        $parcel = $this->make(Parcel::class);
+
+        /** @var Resident $resident */
+        $resident = $this->make(Resident::class, ['parcel' => $parcel]);
+
+        /** @var Allowance $allowance */
+        $allowance = $this->make(Allowance::class, ['resident' => $resident]);
+
         /** @var Decision $decision */
-        $decision = $this->make(Decision::class, ['budget' => 100]);
+        $decision = $this->make(Decision::class, ['allowance' => $allowance, 'budget' => 100]);
+
 
         /** @var Taxi $taxi */
-        $taxi = $this->make(Taxi::class);
+        $taxi = $this->make(Taxi::class, ['parcel' => $parcel]);
 
         $this->ride
             ->setLocation($location)
