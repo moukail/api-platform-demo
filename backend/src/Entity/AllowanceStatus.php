0c26f0538ab99faa93f5f999f8aa9591db087a69
imoukafih <i.moukafih@gmail.com>
2022-10-11 23:13:58 +0200

    * Test cant make ride with low budget * Test cant make ride with different parcel

--- backend/src/Entity/AllowanceStatus.php
+++ backend/src/Entity/AllowanceStatus.php
@@ -7,4 +7,13 @@ enum AllowanceStatus: string
     case PENDING = 'pending';
     case STOPPED = 'stopped';
     case ACTIVE = 'active';
+
+    public function label(): string
+    {
+        return match($this) {
+            static::PENDING => 'Pending',
+            static::ACTIVE => 'Active',
+            static::STOPPED => 'Stopped',
+        };
+    }
 }
